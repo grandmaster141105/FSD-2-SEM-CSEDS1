b2448374e7caf770177d4a1d4691f7b81e5651c9
shreya singh <ershreya.vns@gmail.com>
2026-05-21 12:57:55 +0530

    experiments

---
+++ index.js
@@ -0,0 +1,13 @@
+//greet()
+//console.log(greet);
+function add (a,b){
+    return a+b;
+}
+function greet (){
+    console.log("Hello first Year");
+}
+greet()
+console.log(greet);
+const greet =() => {
+    console.log("Hello first Year");
+}
---
+++ variableHoisting.js
@@ -0,0 +1,7 @@
+//console.log(a);//here the memory is undefined so it will print undefined 
+var a =24;
+console.log(a);
+
+//console.log(b);
+let b=24;
+console.log(b
